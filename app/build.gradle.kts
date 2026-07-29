e3f326bfabca22186766ff4eb462b097cc5d236c
zixij644-elaborate <zixij644@gmail.com>
2026-07-29 11:46:04 +0800

    v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.pickupcode.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.2"
+        versionCode = 12
+        versionName = "1.0.1"
     }
 
     buildTypes {
